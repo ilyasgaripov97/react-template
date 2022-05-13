3a03478f31a30b64b7b5cb7f5de692bbd2a1b304
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-05-13 20:51:41 +0300

    Add react testing library to template.

--- test/App.test.js
+++ test/App.test.js
@@ -1,3 +1,9 @@
-test('1 is equal 1', () => {
-  expect(1).toBe(1);
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../src/components/App/App';
+
+test('renders React App', () => {
+  render(<App/>);
+  const headerElement = screen.getByText(/React App/i);
+  expect(headerElement).toBeInTheDocument();
 });
