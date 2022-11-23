5902288dadd5a12cdfd68e17159d0dd5d5db8fc2
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-11-23 09:50:43 +0300

    Add css dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
         test: /\.ts$|tsx/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   resolve: {
